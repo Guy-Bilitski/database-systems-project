93d147f1314f7209207d28d03a0b6aa239b0d160
Guy-Bilitski <guyb181@gmail.com>
2023-01-09 21:47:14 +0200

    update

--- data_getter.py
+++ data_getter.py
@@ -1,5 +1,6 @@
 import requests
-
+import re
+import json
 
 def get_games(url, headers):
     res = requests.get(url, headers=headers)
@@ -13,9 +14,15 @@ def main():
         auth = f.readline()
 
     headers["Authorization"] = auth
-    res = get_games(url, headers)
-    print(res)
-    print("yes")
+    text_res = get_games(url, headers).text
+    res_list = re.split('{*}', text_res)
+    res_list = list(map(lambda x: x[1:]+'}', res_list))[:-1]
+    res_list = list(map(lambda x: json.loads(x), res_list))
+    print(res_list[len(res_list) - 1])
+    #for item in res_list:
+    #    print(item)
+    #    print(json.loads(item))
+    print(text_res)
 
 
 if __name__ == "__main__":
