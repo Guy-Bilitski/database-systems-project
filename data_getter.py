5abb06005a4948715f803c09092fe2119d3d47c4
Guy-Bilitski <guyb181@gmail.com>
2023-01-11 19:54:42 +0200

    update

--- data_getter.py
+++ data_getter.py
@@ -1,26 +1,38 @@
 import requests
 import re
 import json
-import pyodbc
+import mysql.connector
 
 HEADERS = {"accept": "application/json"}
-SQL_SERVER = "localhost"
+HOST = "localhost"
 DB_NAME = "guybilitski"
 DRIVER = "{SQL Server}"
+PORT = "3305"
+
+VENUES_URL = "https://api.collegefootballdata.com/venues"
+GAMES_URL = lambda year: 'https://api.collegefootballdata.com/games?year={}&seasonType=regular'.format(year)
+
+GAMES_INSERT_QUERY = "INSERT INTO Games" \
+                     " (ID, Season, Week, Neutral_site, Attendance, Venue_id, home_id, home_points, " \
+                     "Home_post_win_prob, Away_id, Away_points, Away_post_win_prob, Excitement_index)" \
+                     " VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+VENUES_INSERT_QUERY = "INSERT INTO Venues" \
+                      " (ID, Name, Capacity, Grass, City, State)" \
+                      " VALUES (%s, %s, %s, %s, %s, %s)"
 
 
 def connect_to_db(uid, pwd):
-    config = "DRIVER={};SERVER={};DATABASE={};UID={};PWD={}"\
-        .format(DRIVER, SQL_SERVER, DB_NAME, uid, pwd)
-    conn = pyodbc.connect(config, timeout=2)
-    return conn.cursor()
+    cnx = mysql.connector.connect(
+        host=HOST,
+        port=PORT,
+        user=uid,
+        password=pwd,
+        database=DB_NAME
+    )
+    return cnx
 
 
-def games_url(year):
-    return 'https://api.collegefootballdata.com/games?year={}&seasonType=regular'.format(year)
-
-
-def get_games(url, headers):
+def get_docs(url, headers):
     res = requests.get(url, headers=headers)
     text_res = res.text
     res_list = re.split('{*}', text_res)
@@ -29,13 +41,29 @@ def get_games(url, headers):
     return res_list
 
 
-def insert_games(start_year, finish_year):
+def insert_venues(cnx):
+    cursor = cnx.cursor()
+    venues = get_docs(VENUES_URL, HEADERS)
+    for i, doc in enumerate(venues):
+        doc_args = (doc["id"], doc["season"], doc["week"], doc["neutral_site"], doc["attendance"],
+                    doc["venue_id"], doc["home_id"], doc["home_points"], doc["home_post_win_prob"],
+                    doc["away_id"], doc["away_points"], doc["away_post_win_prob"], doc["excitement_index"])
+        cursor.execute(GAMES_INSERT_QUERY, doc_args)
+        if i % 50 == 0:
+            cnx.commit()
+    cnx.commit()
 
-    for year in range(start_year, finish_year+1):
-        api_url = games_url(year)
-        games = get_games(api_url, HEADERS)
+
+def insert_games(start_year, finish_year, cnx):
+    cursor = cnx.cursor()
+    for year in range(start_year, finish_year + 1):
+        games = get_docs(GAMES_URL(year), HEADERS)
         for i, doc in enumerate(games):
-            pass
+            doc_args = (doc["id"], doc["name"], doc["capacity"], doc["grass"], doc["city"], doc["state"])
+            cursor.execute(VENUES_INSERT_QUERY, doc_args)
+            if i % 50 == 0:
+                cnx.commit()
+        cnx.commit()
 
 
 def main():
@@ -44,10 +72,11 @@ def main():
         uid = f.readline()[:-1]
         pwd = f.readline()
 
-    db = connect_to_db(uid, pwd)
     HEADERS["Authorization"] = auth
-    # insert_games(1990, 2020)
 
+    cnx = connect_to_db(uid, pwd)
+    insert_venues(cnx)
+    #insert_games(1990, 2020, cnx)
 
 
 if __name__ == "__main__":
