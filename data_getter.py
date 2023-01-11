7c72f1ee44c452e6bd265c493ced3823c89e0b24
Guy-Bilitski <guyb181@gmail.com>
2023-01-11 18:58:25 +0200

    update

--- data_getter.py
+++ data_getter.py
@@ -1,28 +1,53 @@
 import requests
 import re
 import json
+import pyodbc
+
+HEADERS = {"accept": "application/json"}
+SQL_SERVER = "localhost"
+DB_NAME = "guybilitski"
+DRIVER = "{SQL Server}"
+
+
+def connect_to_db(uid, pwd):
+    config = "DRIVER={};SERVER={};DATABASE={};UID={};PWD={}"\
+        .format(DRIVER, SQL_SERVER, DB_NAME, uid, pwd)
+    conn = pyodbc.connect(config, timeout=2)
+    return conn.cursor()
+
+
+def games_url(year):
+    return 'https://api.collegefootballdata.com/games?year={}&seasonType=regular'.format(year)
+
 
 def get_games(url, headers):
     res = requests.get(url, headers=headers)
-    return res
+    text_res = res.text
+    res_list = re.split('{*}', text_res)
+    res_list = list(map(lambda x: x[1:] + '}', res_list))[:-1]
+    res_list = list(map(lambda x: json.loads(x), res_list))
+    return res_list
+
+
+def insert_games(start_year, finish_year):
+
+    for year in range(start_year, finish_year+1):
+        api_url = games_url(year)
+        games = get_games(api_url, HEADERS)
+        for i, doc in enumerate(games):
+            pass
 
 
 def main():
-    url = "https://api.collegefootballdata.com/games?year=2020&seasonType=regular"
-    headers = {"accept": "application/json"}
     with open('secret.txt') as f:
-        auth = f.readline()
+        auth = f.readline()[:-1]
+        uid = f.readline()[:-1]
+        pwd = f.readline()
 
-    headers["Authorization"] = auth
-    text_res = get_games(url, headers).text
-    res_list = re.split('{*}', text_res)
-    res_list = list(map(lambda x: x[1:]+'}', res_list))[:-1]
-    res_list = list(map(lambda x: json.loads(x), res_list))
-    print(res_list[len(res_list) - 1])
-    #for item in res_list:
-    #    print(item)
-    #    print(json.loads(item))
-    print(text_res)
+    db = connect_to_db(uid, pwd)
+    HEADERS["Authorization"] = auth
+    # insert_games(1990, 2020)
+
 
 
 if __name__ == "__main__":
